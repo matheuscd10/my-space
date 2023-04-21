1764cf1aac231d43fcf6b82d877e12f7a9dd7c44
Matheus Chagas Dias <matheuscd10@gmail.com>
2023-04-21 13:17:39 -0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,193 @@
+<template>
+  <v-layout column justify-center align-center class="mt-4 pt-2">
+    <!--<VueCompareImage
+      class="hidden-md-and-down"
+      hover
+      :style="{ minWidth: '1300px' }"
+      :sliderLineWidth="sliderLine"
+      :handleSize="hSize"
+      :leftImage="leftImage"
+      :rightImage="rightImage"
+      :sliderPositionPercentage="sliderPosition"
+    />
+    <VueCompareImage
+      class="hidden-lg-and-up"
+      hover
+      :style="{ maxWidth: '400px' }"
+      :sliderLineWidth="sliderLine"
+      :handleSize="hSize"
+      :leftImage="leftImage2"
+      :rightImage="rightImage2"
+      :sliderPositionPercentage="sliderPosition"
+    />-->
+
+    <br>
+
+    <vue-typer class="headline" :repeat="0" text="Matheus Chagas Dias"></vue-typer>
+    <vue-typer
+      :text="text1"
+      :repeat="Infinity"
+      :shuffle="false"
+      initial-action="erasing"
+      :pre-type-delay="70"
+      :type-delay="70"
+      :pre-erase-delay="1980"
+      :erase-delay="150"
+      erase-style="select-back"
+      :erase-on-complete="false"
+      caret-animation="smooth"
+    ></vue-typer>
+    <br>
+    <v-flex xs12 sm6 md4 lg4 xl4>
+      <v-card flat color="transparent" max-width="500">
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">
+              <span>About</span>
+              <span class="green--text"> Me</span>
+            </h3>
+            <div>
+              <p>
+                Hello! I’m Matheus Chagas Dias. I'm a fullstack developer who
+                is passionate about
+                <span
+                  class="green--text font-weight-bold"
+                >CODING</span>, building great user experiences,
+                fighting for simplicity over complexity and constantly learning.
+                When I'm not coding, you'll find me playing games or making friends.
+              </p>
+            </div>
+          </div>
+        </v-card-title>
+
+        <v-card-actions class="hidden-sm-and-down justify-center text-center">
+          <v-btn
+            v-for="icon in icons"
+            :key="icon.icon"
+            fab
+            dark
+            outline
+            color="green"
+            :href="icon.href"
+            target="_blank"
+          >
+            <v-icon dark>{{icon.icon}}</v-icon>
+          </v-btn>
+        </v-card-actions>
+
+        <v-card-actions class="hidden-md-and-up justify-center text-center">
+          <v-btn
+            v-for="icon in icons"
+            :key="icon.icon"
+            small
+            fab
+            dark
+            outline
+            color="green"
+            :href="icon.href"
+            target="_blank"
+          >
+            <v-icon dark>{{icon.icon}}</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import { VueTyper } from "vue-typer";
+import VueCompareImage from "vue-compare-image";
+
+export default {
+  metaInfo: {
+    title: "Home",
+    titleTemplate: "%s ← Matheus Space",
+    meta: [
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "description", content: "Matheus C. Dias Portfolio" },
+      { charset: "utf-8" },
+      { property: "og:title", content: "Matheus Space" },
+      { property: "og:site_name", content: "Matheus Space" },
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://eldin.space" },
+      {
+        property: "og:image",
+        content: "https://i.imgur.com/Dcz2PGx.jpg"
+      },
+      { property: "og:description", content: "Matheus C. Dias Portfolio" }
+    ]
+  },
+  components: {
+    "vue-typer": VueTyper,
+    VueCompareImage
+  },
+  data() {
+    return {
+      icons: [
+        { href: "https://github.com/matheuscd10", icon: "fab fa-github" },
+        /*{
+          href: "https://stackoverflow.com/users/story/9660700",
+          icon: "fab fa-stack-overflow"
+        },*/
+        { href: "https://twitter.com/matheus292002", icon: "fab fa-twitter" },
+        {
+          href: "https://www.linkedin.com/in/matheus-chagas-dias-067a43224",
+          icon: "fab fa-linkedin-in"
+        },
+        /*{
+          href: "https://www.xing.com/profile/Eldin_Zaimovic?sc_o=mxb_p",
+          icon: "fab fa-xing"
+        },*/
+        {
+          href: "https://www.facebook.com/matheus.c.dias.1",
+          icon: "fab fa-facebook-f"
+        },
+        /*{ href: "https://instagram.com/matheus_c_dias?igshid=YmMyMTA2M2Y=", icon: "fab fa-instagram" }*/
+      ],
+      text1: ["Computer Engineer Student", "Fullstack Developer", "Freelancer"],
+      leftImage: "https://i.imgur.com/bU50uS9.jpg",
+      rightImage: "https://i.imgur.com/z3ZoEEw.jpg",
+      leftImage2: "https://i.imgur.com/SXBitjh.jpg",
+      rightImage2: "https://i.imgur.com/Dcz2PGx.jpg",
+      sliderLine: 0,
+      hSize: 0,
+      sliderPosition: 0.5
+    };
+  }
+};
+</script>
+<style>
+@keyframes rocking {
+  0%,
+  100% {
+    transform: rotateZ(-10deg);
+  }
+
+  50% {
+    transform: rotateZ(10deg);
+  }
+}
+
+.vue-typer {
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+}
+.vue-typer .custom.char.typed {
+  color: green;
+}
+.vue-typer .custom.char.selected {
+  color: #e91e63;
+}
+
+.vue-typer .custom.caret {
+  animation: rocking 1s ease-in-out 0s infinite;
+}
+.vue-typer .custom.caret.typing {
+  background-color: green;
+}
+.vue-typer .custom.caret.selecting {
+  display: inline-block;
+  background-color: #e91e63;
+}
+</style>
+
---
+++ src/views/Resume.vue
@@ -0,0 +1,151 @@
+<template>
+  <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+    <v-flex xs12 sm12 md5 lg6 xl6 class="mt-4 pt-4">
+      <h2 class="mb-4 pl-4 pt-2">Education</h2>
+      <v-card-text class="py-0">
+        <v-timeline align-top dense>
+          <v-timeline-item color="green" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold green--text">JAN 2020 -</div>
+                <div class="caption font-weight-bold green--text">PRESENT</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Computer Engineering</strong>
+                <div class="caption mb-2">Unoesc</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+        </v-timeline>
+      </v-card-text>
+    </v-flex>
+    <v-flex xs12 sm12 md4 lg6 xl6 class="mt-4 pt-2">
+      <h2 class="mb-4 pl-4">Experience</h2>
+      <v-card-text class="py-0">
+        <v-timeline align-top dense>
+          <v-timeline-item color="green" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold green--text">JAN 2023 -</div>
+                <div class="caption font-weight-bold green--text">PRESENT</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Looking for first experience as a developer</strong>
+                <div class="caption mb-2">Remote</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+        </v-timeline>
+      </v-card-text>
+    </v-flex>
+    <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+      <v-flex xs12 sm12 md4 lg5 xl5 class="mx-2 pt-4">
+        <h2>
+          <span>Coding</span>
+          <span class="green--text"> Skills</span>
+        </h2>
+        <br>
+        <strong>Python</strong>
+        <v-progress-linear v-model="buffer1" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <strong>C++/C#</strong>
+        <v-progress-linear v-model="buffer2" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <strong>JavaScript</strong>
+        <v-progress-linear v-model="buffer3" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+      </v-flex>
+      <v-flex xs12 sm12 md4 lg5 xl5 class="mx-2 pt-4">
+        <h2>
+          <span>General</span>
+
+          <span class="green--text"> Skills</span>
+        </h2>
+        <br>
+        <strong>HTML/CSS</strong>
+        <v-progress-linear v-model="buffer4" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <strong>SQL</strong>
+        <v-progress-linear v-model="buffer5" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <strong>PostgreSQL</strong>
+        <v-progress-linear v-model="buffer6" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+      </v-flex>
+      <div class="mt-4 pt-2">
+        <v-btn
+          href="https://drive.google.com/file/d/1jFfc_6CWf15686sMUrrsOzZJu_LBYjRH/view?usp=share_link"
+          target="_blank"
+          color="green"
+          dark
+          large
+        >
+          Download Resume
+          <v-icon right dark>fas fa-file-download</v-icon>
+        </v-btn>
+      </div>
+    </v-layout>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Resume",
+    titleTemplate: "%s ← Matheus Space",
+    meta: [
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        name: "description",
+        content:
+          "Vue NUXT HTML CSS JavaScript Design Adobe XD Photoshop Coding Skills Resume Work Web Developer Front-End"
+      },
+      { charset: "utf-8" },
+      { property: "og:title", content: "Matheus Space" },
+      { property: "og:site_name", content: "Matheus Space" },
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://eldin.space" },
+      {
+        property: "og:image",
+        content: "https://i.imgur.com/Dcz2PGx.jpg"
+      },
+      {
+        property: "og:description",
+        content:
+          "Vue NUXT HTML CSS JavaScript Design Adobe XD Photoshop Coding Skills Resume Work Web Developer Front-End"
+      }
+    ]
+  },
+  data() {
+    return {
+      value: 0,
+      buffer1: 0,
+      buffer2: 0,
+      buffer3: 0,
+      buffer4: 0,
+      buffer5: 0,
+      buffer6: 0,
+      bufferValue: 100,
+      interval: 0
+    };
+  },
+  mounted() {
+    this.startBuffer();
+  },
+
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+
+  methods: {
+    startBuffer() {
+      this.interval = setInterval(() => {
+        this.buffer1 = 90;
+        this.buffer2 = 85;
+        this.buffer3 = 85;
+        this.buffer4 = 75;
+        this.buffer5 = 70;
+        this.buffer6 = 70;
+      }, 800);
+    }
+  }
+};
+</script>
+<style >
+</style>
+
